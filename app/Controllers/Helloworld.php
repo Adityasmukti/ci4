7142a23985353fef1c26939e4f243f816ae62620
Adityas Muktiyanto <adityasmukti@outlook.com>
2020-05-16 14:22:51 +0700

    init

---
+++ app/Controllers/Helloworld.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Helloworld extends BaseController
+{
+  public function index()
+  {
+    echo "Hello World";
+  }
+
+  //--------------------------------------------------------------------
+
+}
